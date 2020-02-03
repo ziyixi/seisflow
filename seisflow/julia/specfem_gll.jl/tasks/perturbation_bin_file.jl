f9716bfe9a83dd7e03b0988c13f412ffbebccbe8
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-02-03 15:21:11 -0500

    add progress bar support for getting the model perturbation

--- seisflow/julia/specfem_gll.jl/tasks/perturbation_bin_file.jl
+++ seisflow/julia/specfem_gll.jl/tasks/perturbation_bin_file.jl
@@ -1,5 +1,6 @@
 include("../src/utils/readfiles.jl")
 include("../src/setting/constants.jl")
+using ProgressMeter
 
 """
 generate perturbation file according to the reference bin file
@@ -9,10 +10,7 @@ function generate_perturbation(target_basedir::String, reference_basedir::String
     model_gll_reference = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_target = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_output = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
-    for iproc in 0:nproc - 1
-        if(iproc % 10 == 0)
-            @info "step" iproc
-        end
+    @showprogress for iproc in 0:nproc - 1
         for tag in tags_splitted
             sem_io_read_gll_file_1!(reference_basedir, iproc, tag, model_gll_reference)
             sem_io_read_gll_file_1!(target_basedir, iproc, tag, model_gll_target)
@@ -32,10 +30,7 @@ function generate_real(target_basedir::String, reference_basedir::String, output
     model_gll_reference = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_target = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_output = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
-    for iproc in 0:nproc - 1
-        if(iproc % 10 == 0)
-            @info "step" iproc
-        end
+    @showprogress for iproc in 0:nproc - 1
         for tag in tags
             sem_io_read_gll_file_1!(reference_basedir, iproc, tag, model_gll_reference)
             sem_io_read_gll_file_1!(target_basedir, iproc, tag, model_gll_target)
