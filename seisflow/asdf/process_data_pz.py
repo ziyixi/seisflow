199e8d158c4c7b1b47aa45158f0e5776fac6df8d
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-02-20 16:20:08 -0500

    debug

--- seisflow/asdf/process_data_pz.py
+++ seisflow/asdf/process_data_pz.py
@@ -191,7 +191,8 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
 
             # overlap the previous trace
             status_code = check_st_numberlap(st, inv)
-            if(status_code == -1):
+            if (status_code == -1):
+                print("a")
                 return
             elif(status_code == 0):
                 pass
@@ -200,6 +201,7 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
                 try:
                     st.merge(method=1, fill_value=0, interpolation_samples=0)
                 except:
+                    print("a")
                     return
             else:
                 raise Exception("unknown status code")
@@ -207,7 +209,8 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
             status_code = check_time(st, event_time, waveform_length, inv)
             if(status_code == 0):
                 pass
-            elif(status_code == -1):
+            elif (status_code == -1):
+                print("b")
                 return
             else:
                 raise Exception("unknown status code")
@@ -224,7 +227,8 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
             st.detrend("demean")
             st.detrend("linear")
 
-            if(st == None):
+            if (st == None):
+                print("c")
                 return
             # sac has already removed mean after removing the response.
 
@@ -242,7 +246,8 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
             if(correct_cea and (network in CEA_NETWORKS)):
                 baz = func_correct_cea(
                     baz, inv, event_time, correction_data)
-            if(baz == None):
+            if (baz == None):
+                print("d")
                 return
 
             # we have to limit baz to be in [0,360)
@@ -254,8 +259,10 @@ def process_single_event(min_periods, max_periods, taper_tmin_tmax, asdf_filenam
                 try:
                     st.rotate(method="NE->RT", back_azimuth=baz)
                 except:
+                    print("e")
                     return
             else:
+                print("f")
                 return
             # bandpass filter
             st.filter("bandpass", freqmin=1.0/max_period,
