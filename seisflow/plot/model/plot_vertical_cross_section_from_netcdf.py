c121bbcdab27d4a1298ee3406b73a9a6a73509ca
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-03-24 18:06:20 -0400

    add flat plot for the vertical crosssection

--- seisflow/plot/model/plot_vertical_cross_section_from_netcdf.py
+++ seisflow/plot/model/plot_vertical_cross_section_from_netcdf.py
@@ -55,26 +55,38 @@ def extract_data_v(netcdf_data, lon1, lat1, lon2, lat2, dep1, dep2, rh, rdep, th
     return mesh_theta, mesh_dep, plot_values
 
 
-def plot_v(lat1, lat2, lon1, lon2, dep2, theta_label, mesh_theta, mesh_dep, plot_values, vmin, vmax):
+def plot_v(lat1, lat2, lon1, lon2, dep1, dep2, theta_label, mesh_theta, mesh_dep, plot_values, vmin, vmax, flat):
     fig = plt.figure()
-    ax = fig.add_subplot(111, polar=True)
-    if(theta_label == "lat"):
-        lat2, lat1 = min(lat1, lat2), max(lat1, lat2)
-        ax.set_thetamin(lat2)
-        ax.set_thetamax(lat1)
-        ax.set_theta_zero_location("N", offset=-(lat1+lat2)/2)
+    if (flat):
+        ax = fig.add_subplot(111)
+        if(theta_label == "lat"):
+            lat2, lat1 = min(lat1, lat2), max(lat1, lat2)
+            ax.set_xlim([lat2, lat1])
+        else:
+            lon2, lon1 = min(lon1, lon2), max(lon1, lon2)
+            ax.set_xlim([lon2, lon1])
+        ax.set_ylim([dep1, dep2])
+        contourf_ = ax.pcolormesh(
+            mesh_theta, mesh_dep, plot_values, cmap=plt.cm.jet_r, vmin=vmin, vmax=vmax)  # pylint: disable=no-member
+        ax.set_ylim(ax.get_ylim()[::-1])
+        plt.colorbar(contourf_, orientation='horizontal',
+                     fraction=0.046, pad=0.1)
     else:
-        lon2, lon1 = min(lon1, lon2), max(lon1, lon2)
-        ax.set_thetamin(lon2)
-        ax.set_thetamax(lon1)
-        ax.set_theta_zero_location("N", offset=-(lon1+lon2)/2)
-    ax.set_rorigin(6371)
-    ax.set_rlim(bottom=dep2, top=0)
-    plt.subplots_adjust(bottom=0.2)
-
-    contourf_ = ax.pcolormesh(np.deg2rad(
-        mesh_theta), mesh_dep, plot_values, cmap=plt.cm.jet_r, vmin=vmin, vmax=vmax)  # pylint: disable=no-member
-    plt.colorbar(contourf_, orientation='horizontal', fraction=0.046, pad=0.0)
+        ax = fig.add_subplot(111, polar=True)
+        if(theta_label == "lat"):
+            lat2, lat1 = min(lat1, lat2), max(lat1, lat2)
+            ax.set_thetamin(lat2)
+            ax.set_thetamax(lat1)
+            ax.set_theta_zero_location("N", offset=-(lat1+lat2)/2)
+        else:
+            lon2, lon1 = min(lon1, lon2), max(lon1, lon2)
+            ax.set_thetamin(lon2)
+            ax.set_thetamax(lon1)
+            ax.set_theta_zero_location("N", offset=-(lon1+lon2)/2)
+        contourf_ = ax.pcolormesh(np.deg2rad(
+            mesh_theta), mesh_dep, plot_values, cmap=plt.cm.jet_r, vmin=vmin, vmax=vmax)  # pylint: disable=no-member
+        plt.colorbar(contourf_, orientation='horizontal',
+                     fraction=0.046, pad=0.0)
 
     plt.show()
 
@@ -88,7 +100,8 @@ def plot_v(lat1, lat2, lon1, lon2, dep2, theta_label, mesh_theta, mesh_dep, plot
 @click.option('--rh', required=True, type=float, help="the horizontal resolution")
 @click.option('--rdep', required=True, type=float, help="the vertical resolution")
 @click.option('--theta_label', required=True, type=str, help="can be lat or lon")
-def main(netcdf_file, parameter, vmin, vmax, region, rh, rdep, theta_label):
+@click.option('--flat/--no-flat', default=False, required=False, help="if plot the flat cross section")
+def main(netcdf_file, parameter, vmin, vmax, region, rh, rdep, theta_label, flat):
     with netcdf.netcdf_file(netcdf_file, 'r') as f:
         interpolating_function = get_interp_function(
             f, parameter, method="linear")
@@ -96,8 +109,8 @@ def main(netcdf_file, parameter, vmin, vmax, region, rh, rdep, theta_label):
         mesh_theta, mesh_dep, array_to_interpolate = generate_vcs_mesh(
             lon1, lat1, lon2, lat2, dep1, dep2, rh, rdep, theta_label)
         plot_values = interpolating_function(array_to_interpolate)
-        plot_v(lat1, lat2, lon1, lon2, dep2, theta_label,
-               mesh_theta, mesh_dep, plot_values, vmin, vmax)
+        plot_v(lat1, lat2, lon1, lon2, dep1, dep2, theta_label,
+               mesh_theta, mesh_dep, plot_values, vmin, vmax, flat)
 
 
 if __name__ == "__main__":
