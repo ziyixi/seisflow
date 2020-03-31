aaa1a5141942f3a31406da782372488a3eee93f5
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-03-31 14:54:49 -0400

    update mask script

--- seisflow/dev/generate_source_mask_list.py
+++ seisflow/dev/generate_source_mask_list.py
@@ -4,22 +4,32 @@ generate the source mask list.
 from glob import glob
 from os.path import join
 
+import click
 import numpy as np
 
 
 @click.command()
 @click.option('--base_directory', required=True, type=str, help="the base simulation directory")
-@click.option('--output_path', required=True, type=str, help="the output path for the source mask list")
-def main(base_directory, output_path):
+@click.option('--source_mask', required=True, type=float, help="radius for source mask in km")
+@click.option('--receiver_mask', required=True, type=float, help="radius for receiver mask in km")
+def main(base_directory, source_mask, receiver_mask):
     all_event_dirs = glob(join(base_directory, "*"))
-    source_mask_list = []
     for each_event_dir in all_event_dirs:
         source_mask_vtk_path = join(
             each_event_dir, "OUTPUT_FILES", "source.vtk")
+        receiver_mask_vtk_path = join(
+            each_event_dir, "OUTPUT_FILES", "receiver.vtk")
         source_mask = np.loadtxt(source_mask_vtk_path, skiprows=5)
-        source_mask_list.append(source_mask)
-    source_mask_list = np.array(source_mask_list)
-    np.savetxt(output_path, source_mask_list)
+        source_mask_append = np.ones((source_mask.shape[0], 1)) * source_mask
+        source_mask = np.hstack([source_mask, source_mask_append])
+        receiver_mask = np.loadtxt(receiver_mask_vtk_path, skiprows=5)
+        receiver_mask_append = np.ones(
+            (receiver_mask.shape[0], 1)) * receiver_mask
+        receiver_mask = np.hstack([receiver_mask, receiver_mask_append])
+        mask_list = np.vstack([source_mask, receiver_mask])
+        output_path = join(
+            each_event_dir, "OUTPUT_FILES", "mask.xyz")
+        np.savetxt(output_path, mask_list)
 
 
 if __name__ == "__main__":
