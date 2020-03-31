1c4851e0ffe1095f66ac99950790d40f337309c7
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-03-31 15:46:41 -0400

    add script to generate make gaussian mask func

--- seisflow/dev/make_gaussian_mask.py
+++ seisflow/dev/make_gaussian_mask.py
@@ -2,7 +2,7 @@
 Generate the string for making the gaussian mask.
 """
 from glob import glob
-from os.path import join
+from os.path import join, basename
 
 
 def make_gaussian_mask(base_dir, sem_bin_dir, nproc):
@@ -10,10 +10,11 @@ def make_gaussian_mask(base_dir, sem_bin_dir, nproc):
     command = join(sem_bin_dir, "xsem_make_gaussian_mask")
     result = ""
     result += "module load netcdf; "
-    for each_event_dir in all_event_dirs:
+    for index, each_event_dir in enumerate(all_event_dirs):
+        gcmtid = basename(each_event_dir)
         mesh_dir = join(each_event_dir, "DATABASES_MPI")
         source_xyz_list = join(each_event_dir, "OUTPUT_FILES", "mask.xyz")
         out_dir = join(each_event_dir, "DATABASES_MPI")
         out_name = "mask_source"
-        result += f"ibrun {command} {nproc} {mesh_dir} {source_xyz_list} {out_dir} {out_name}; "
+        result += f"echo 'mask source for iter{index}:{gcmtid}' ; ibrun {command} {nproc} {mesh_dir} {source_xyz_list} {out_dir} {out_name}; "
     return result
