b7de9926a8152041bdbeb65c5550192aa3820ca1
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-04-26 13:44:45 -0400

    make xs cmt perturbation the same

--- seisflow/tasks/cmtsolution/make_perturbed_cmtsolution.py
+++ seisflow/tasks/cmtsolution/make_perturbed_cmtsolution.py
@@ -73,10 +73,12 @@ def add_src_frechet(src_frechet, cmtsolution, max_dxs_ratio, fix_location=False,
     y += dxs_xyz[1]
     z += dxs_xyz[2]
     lon, lat, alt = pyproj.transform(ecef, lla, x, y, z)
-    # add dmt
-    mt += dmt
+    # # add dmt
+    # mt += dmt
     # we have to get mt at the new position
     mt_xyz = np.dot(np.dot(a, mt), np.transpose(a))
+    dmt_xyz = np.dot(np.dot(a, dmt), np.transpose(a))
+    mt_xyz += dmt_xyz
     # get new a at new x,y,z
     r = (x**2 + y**2 + z**2)**0.5
     theta = np.arccos(z/r)
