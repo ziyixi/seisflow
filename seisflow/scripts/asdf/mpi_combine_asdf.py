cc9db7061b2321f977126c77b51f28d977439754
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-05-30 13:23:15 -0400

    update the script of combining asdf

--- seisflow/scripts/asdf/mpi_combine_asdf.py
+++ seisflow/scripts/asdf/mpi_combine_asdf.py
@@ -2,13 +2,14 @@
 mpi_combine_asdf.py: combine two asdf files together.
 """
 from glob import glob
-from os.path import basename, join
+from os.path import basename, join, isfile
 
 import click
 import numpy as np
 from mpi4py import MPI
 
 from ...tasks.asdf.combine_asdf_files import combine_asdf
+import sh
 
 comm = MPI.COMM_WORLD  # pylint: disable=c-extension-no-member
 size = comm.Get_size()
@@ -19,14 +20,17 @@ def get_asdf_pairs(base_directory, append_directory, output_directory):
     """
     The file names in the twp directory should be the same.
     """
+    # * we will always base on the number of the base directory
     all_base_paths = sorted(glob(join(base_directory, "*")))
-    all_append_paths = sorted(glob(join(append_directory, "*")))
-    assert len(all_base_paths) == len(all_append_paths)
     pair_list = []
-    for each_base_path, each_append_path in zip(all_base_paths, all_append_paths):
-        assert basename(each_base_path) == basename(each_append_path)
-        output_path = join(output_directory, basename(each_base_path))
-        pair_list.append((each_base_path, each_append_path, output_path))
+    for each_base_path in all_base_paths:
+        thebase = basename(each_base_path)
+        each_append_path = join(append_directory, thebase)
+        output_path = join(output_directory, thebase)
+        if (isfile(each_append_path)):
+            pair_list.append((each_base_path, each_append_path, output_path))
+        else:
+            pair_list.append((each_base_path, output_path))
     return pair_list
 
 
@@ -44,7 +48,12 @@ def main(base_directory, append_directory, output_directory):
         base_directory, append_directory, output_directory)
     pair_list_this_rank = get_asdf_pairs_this_rank(pair_list)
     for each_pair in pair_list_this_rank:
-        combine_asdf(each_pair[0], each_pair[1], each_pair[2])
+        if (len(each_pair) == 2):
+            sh.cp(each_pair[0], each_pair[1])
+        elif(len(each_pair) == 3):
+            combine_asdf(each_pair[0], each_pair[1], each_pair[2])
+        else:
+            raise Exception("len(each_pair) is not 2 or 3")
 
 
 if __name__ == "__main__":
