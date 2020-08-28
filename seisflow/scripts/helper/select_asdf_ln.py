c39f7ac80d3d0b13a03911293929ec7f05d56fcd
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-08-28 14:35:16 -0400

    fix bug

--- seisflow/scripts/helper/select_asdf_ln.py
+++ seisflow/scripts/helper/select_asdf_ln.py
@@ -15,14 +15,13 @@ def main(cmt_directory, asdf_directory, output_directory):
     all_cmt_files = glob(join(cmt_directory, "*"))
     for each_cmt_file in all_cmt_files:
         gcmtid = basename(each_cmt_file)
-        if(gcmtid not in cp_list):
-            from_path = join(asdf_directory, f"{gcmtid}.h5")
-            to_path = join(output_directory, f"{gcmtid}.h5")
-            command = f"ln -s {from_path} {to_path} "
-            try:
-                subprocess.call(command, shell=True)
-            except sh.ErrorReturnCode_1:
-                pass
+        from_path = join(asdf_directory, f"{gcmtid}.h5")
+        to_path = join(output_directory, f"{gcmtid}.h5")
+        command = f"ln -s {from_path} {to_path} "
+        try:
+            subprocess.call(command, shell=True)
+        except sh.ErrorReturnCode_1:
+            pass
 
 
 if __name__ == "__main__":
