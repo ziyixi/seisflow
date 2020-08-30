37e6f3c91bdd308a5f29d668161f94a9f844a282
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-08-30 12:28:53 -0400

    fix ; bug

--- seisflow/scripts/xsede/xsede_make_gaussian_mask.py
+++ seisflow/scripts/xsede/xsede_make_gaussian_mask.py
@@ -1,3 +1,5 @@
+import sys
+
 import click
 
 from ...slurm.submit_job import submit_job
@@ -18,8 +20,9 @@ from ..structure_inversion.make_gaussian_mask import make_gaussian_mask
 @click.option('--receiver_mask_km', required=True, type=float, help="the receiver mask radius in km")
 def main(base_dir, sem_bin_dir, output_dir, nproc, n_node, n_cores, partition, used_time, account, source_mask_km, receiver_mask_km):
     # * firstly we generate mask_xyz files
+    pyexec = sys.executable
     thecommand = ""
-    thecommand += f"python -m seisflow.scripts.structure_inversion.generate_source_mask_list --base_directory {base_dir} --source_mask_km {source_mask_km} --receiver_mask_km {receiver_mask_km} "
+    thecommand += f"{pyexec} -m seisflow.scripts.structure_inversion.generate_source_mask_list --base_directory {base_dir} --source_mask_km {source_mask_km} --receiver_mask_km {receiver_mask_km};"
     thecommand += make_gaussian_mask(base_dir, sem_bin_dir, output_dir, nproc)
     submit_job("generate mask", thecommand,
                n_node, n_cores, partition, used_time, account, "stampede2")
