54e8fa1b49a87e7304fa3bde56d9fe6859b97f15
ziyixi <ziyixi@mail.ustc.edu.cn>
2020-09-07 16:34:09 -0400

    add test scripts to invert for d660 using multiple events

---
+++ seisflow/scripts/internal_topo/line_search_multiple_events.py
@@ -0,0 +1,114 @@
+"""
+line_search.py: do the line search for single event.
+"""
+
+from copy import copy
+
+import click
+import numpy as np
+
+from ...setting import LINE_SEARCH_PERTURBATION_BOUNDARY
+from ...tasks.line_search.line_search_structure import \
+    calculate_weighted_misfit
+from ...utils.asdf_io import VirAsdf
+from ...utils.load_files import load_first_arrival_baz_evdp, load_pickle
+from .update_model_stw105_multiple_events import kernel as update_model_stw105_kernel
+
+
+def get_perturbed_vir_sync(virasdf_raw, virasdf_perturbed, step_length):
+    """
+    get_perturbed_vir_sync: get perturbed virasdf given a step length.
+    """
+    virasdf_vir_perturbed = copy(virasdf_raw)
+    for each_net_sta in virasdf_vir_perturbed.get_waveforms_list():
+        st_raw = virasdf_raw.get_waveforms()[each_net_sta]["st"]
+        st_per = virasdf_perturbed.get_waveforms()[each_net_sta]["st"]
+        st_vir_perturbed = virasdf_vir_perturbed.get_waveforms()[
+            each_net_sta]["st"]
+        for index in range(len(st_vir_perturbed)):
+            st_vir_perturbed[index].data = st_raw[index].data + \
+                (step_length/LINE_SEARCH_PERTURBATION_BOUNDARY) * \
+                (st_per[index].data-st_raw[index].data)
+        virasdf_vir_perturbed.update_st(st_vir_perturbed, each_net_sta)
+    return virasdf_vir_perturbed
+
+
+def kernel(windows_path, data_asdf_path, sync_asdf_path_raw, sync_asdf_path_perturbed, data_info_directory, stations_path,
+           search_range, search_step, lat_range, lon_range, kernel_path, model_path, output_path_d660, output_path_ppm):
+    # * need to split: windows_path data_asdf_path sync_asdf_path_raw sync_asdf_path_perturbed
+    # * kernel path will be used in multiple kernel updating scripts
+    # * prepare to used parameters
+    windows_list = [load_pickle(each_windows_path)
+                    for each_windows_path in windows_path.split(",")]
+    consider_surface = False
+    data_virasdf_body_list = []
+    sync_virasdf_body_raw_list = []
+    sync_virasdf_body_perturbed_list = []
+    for each_data_asdf_path, each_sync_asdf_path_raw, each_sync_asdf_path_perturbed in zip(data_asdf_path.split(","), sync_asdf_path_raw.split(","), sync_asdf_path_perturbed.split(",")):
+        data_virasdf_body_list.append(VirAsdf())
+        data_virasdf_body_list[-1].read_asdf(each_data_asdf_path)
+        sync_virasdf_body_raw_list.append(VirAsdf())
+        sync_virasdf_body_raw_list[-1].read_asdf(each_sync_asdf_path_raw)
+        sync_virasdf_body_perturbed_list.append(VirAsdf)
+        sync_virasdf_body_perturbed_list[-1].read_asdf(
+            each_sync_asdf_path_perturbed)
+    data_virasdf_surface = None
+    sync_virasdf_surface = None
+    first_arrival_zr, first_arrival_t, baz, _ = load_first_arrival_baz_evdp(
+        data_info_directory)
+    stations = np.loadtxt(stations_path, dtype=np.str)
+
+    # * now we can do the line search
+    search_min, search_max = map(float, search_range.split(","))
+    search_list = np.arange(search_min, search_max+search_step, search_step)
+    # * for each searching value, we calculate the misfit
+    weighted_misfit_collection = []
+    for each_search_step in search_list:
+        misfit_all = 0
+        for index in range(len(data_virasdf_body_list)):
+            sync_virasdf_body_per = get_perturbed_vir_sync(
+                sync_virasdf_body_raw_list[index], sync_virasdf_body_perturbed_list[index], each_search_step)
+            misfit_all += calculate_weighted_misfit(windows_list[index], consider_surface, data_virasdf_body_list[index], sync_virasdf_body_per, data_virasdf_surface,
+                                                    sync_virasdf_surface, first_arrival_zr, first_arrival_t, baz, stations)
+        misfit_all = misfit_all / len(data_virasdf_body_list)
+        weighted_misfit_collection.append(misfit_all)
+
+    # * print out the searching result and return the search length
+    print("line search result:")
+    for index, each_search_step in enumerate(search_list):
+        print(
+            f"step: {each_search_step}, misfit: {weighted_misfit_collection[index]}")
+    min_weighted_misfit = np.min(weighted_misfit_collection)
+    optimized_step_length = search_list[np.argmin(
+        weighted_misfit_collection)]
+    print("optimized value:")
+    print(
+        f"step: {optimized_step_length}, misfit: {min_weighted_misfit}")
+    # * now we generate the optimized d660 file and the ppm file.
+    update_model_stw105_kernel(lat_range, lon_range, kernel_path,
+                               model_path, output_path_d660, output_path_ppm, optimized_step=optimized_step_length)
+
+
+@click.command()
+@click.option('--windows_path', required=True, type=str, help="the windows path")
+@click.option('--data_asdf_path', required=True, type=str, help="the data asdf path")
+@click.option('--sync_asdf_path_raw', required=True, type=str, help="the raw processed sync asdf path")
+@click.option('--sync_asdf_path_perturbed', required=True, type=str, help="the perturbed processed sync asdf path")
+@click.option('--data_info_directory', required=True, type=str, help="the data info directory")
+@click.option('--stations_path', required=True, type=str, help="the stations path")
+@click.option('--search_range', required=True, type=str, help="search_min,search_max")
+@click.option('--search_step', required=True, type=float, help="the search step")
+@click.option('--lat_range', required=True, type=str, help="latmin,latmax should be larger than the simulation region")
+@click.option('--lon_range', required=True, type=str, help="lonmin,lonmax should be larger than the simulation region")
+@click.option('--kernel_path', required=True, type=str, help="the generated kernel text file from julia")
+@click.option('--model_path', required=True, type=str, help="the external mesh model file")
+@click.option('--output_path_d660', required=True, type=str, help="the external mesh file output path")
+@click.option('--output_path_ppm', required=True, type=str, help="the external ppm file output path")
+def main(windows_path, data_asdf_path, sync_asdf_path_raw, sync_asdf_path_perturbed, data_info_directory, stations_path,
+         search_range, search_step, lat_range, lon_range, kernel_path, model_path, output_path_d660, output_path_ppm):
+    kernel(windows_path, data_asdf_path, sync_asdf_path_raw, sync_asdf_path_perturbed, data_info_directory, stations_path,
+           search_range, search_step, lat_range, lon_range, kernel_path, model_path, output_path_d660, output_path_ppm)
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
---
+++ seisflow/scripts/internal_topo/update_model_stw105_multiple_events.py
@@ -0,0 +1,107 @@
+"""
+update_model_stw105.py: update the d660 topo and stw105 ppm based on the kernel text file.
+"""
+import click
+import numpy as np
+from scipy.interpolate import griddata
+from scipy.ndimage import gaussian_filter
+
+from ...setting import LINE_SEARCH_PERTURBATION_BOUNDARY
+from .generate_gaussian_stw105 import (generate_mapper, generate_ppm,
+                                       read_stw105, write_to_netcdf)
+
+
+def kernel(lat_range, lon_range, kernel_path,
+           model_path, output_path_d660, output_path_ppm, optimized_step=None):
+    lat_min, lat_max = map(float, lat_range.split(","))
+    lon1 = np.arange(-180, 180.2, 0.2)
+    lat1 = np.arange(lat_min, lat_max + 0.2, 0.2)
+    lon2, lat2 = np.meshgrid(lon1, lat1, indexing="ij")
+
+    input_depth_0_360 = np.zeros_like(lon2)
+    raw_depth = np.loadtxt(model_path, skiprows=1)
+    icount = 0
+    for ilat in range(len(lat1)):
+        for ilon in range(len(lon1)):
+            input_depth_0_360[ilon, ilat] = raw_depth[icount, 3]
+            icount += 1
+    # reshape to the shape from -180 to 180
+    input_depth = np.zeros_like(input_depth_0_360)
+    input_depth[901:, :] = input_depth_0_360[:900, :]
+    input_depth[:901, :] = input_depth_0_360[900:, :]
+    input_depth = input_depth+650
+
+    # * read in the model and kernel
+    kernel_path_list = kernel_path.split(",")
+    input_kernel_all = np.zeros_like(lon2)
+    for each_kernel_path in kernel_path_list:
+        raw_data = np.loadtxt(each_kernel_path)
+        # * interp
+        input_kernel = griddata(raw_data[:, :2], raw_data[:, 3],
+                                (lon2, lat2), method='linear')
+        input_kernel[np.isnan(input_kernel)] = 0
+        # * we should smooth the kernel
+        input_kernel = gaussian_filter(input_kernel, sigma=2)
+        input_kernel_all += input_kernel
+
+    # * we should normalize the kernel so the maximum absolute value can be corresponding to 5km
+    max_abs_pos = np.unravel_index(
+        np.argmax(np.abs(input_kernel_all)), input_kernel_all.shape)
+    if(optimized_step == None):
+        ratio = LINE_SEARCH_PERTURBATION_BOUNDARY / \
+            np.abs(input_kernel_all[max_abs_pos])
+    else:
+        ratio = optimized_step / \
+            np.abs(input_kernel_all[max_abs_pos])
+    # update the kernel based on the ratio
+    output_kernel = ratio * input_kernel_all
+
+    # * add the kernel to the depth
+    output_depth = input_depth + output_kernel
+
+    # * now we write to the topo file, need to change lons from 0 to 360
+    towrite_depth = np.zeros_like(output_depth)
+    towrite_depth[:900, :] = output_depth[901:, :]
+    towrite_depth[900:, :] = output_depth[:901, :]
+    towrite_depth = towrite_depth-650
+    towrite_lon1 = np.arange(0, 360.2, 0.2)
+    towrite_lat1 = lat1
+    with open(output_path_d660, "w") as f:
+        f.write(f"{towrite_depth.shape[0]} {towrite_depth.shape[1]} \n")
+        for ilat in range(len(towrite_lat1)):
+            for ilon in range(len(towrite_lon1)):
+                f.write(
+                    f"{towrite_lon1[ilon]:.2f} {towrite_lat1[ilat]:.2f} 0.0000 {towrite_depth[ilon,ilat]:.4f} \n")
+
+    # * now we are ready to generate the ppm file based in the d660 file
+    latmin, latmax = map(float, lat_range.split(","))
+    lonmin, lonmax = map(float, lon_range.split(","))
+    # based on lonmin and lonmax, we extract the data from output_depth
+    lonmin_index = int((lonmin + 180) * 5)
+    lonmax_index = int((lonmax + 180) * 5)
+    d650 = output_depth[lonmin_index: (lonmax_index + 1), :]
+    d650 = d650-650
+
+    # * now we generate the ppm file
+    stw105_iso = read_stw105()
+    mapper_all, mapper_depression, mapper_uplifting = generate_mapper(
+        d650, stw105_iso)
+    lon1, lat1, dep1, vs3d, vp3d, rho3d = generate_ppm(
+        d650, (latmin, latmax), (lonmin, lonmax), mapper_all, mapper_depression, mapper_uplifting)
+    write_to_netcdf(lon1, lat1, dep1, vs3d, vp3d, rho3d, output_path_ppm)
+
+
+@click.command()
+@click.option('--lat_range', required=True, type=str, help="latmin,latmax should be larger than the simulation region")
+@click.option('--lon_range', required=True, type=str, help="lonmin,lonmax should be larger than the simulation region")
+@click.option('--kernel_path', required=True, type=str, help="the generated kernel text file from julia")
+@click.option('--model_path', required=True, type=str, help="the external mesh model file")
+@click.option('--output_path_d660', required=True, type=str, help="the external mesh file output path")
+@click.option('--output_path_ppm', required=True, type=str, help="the external ppm file output path")
+def main(lat_range, lon_range, kernel_path, model_path, output_path_d660, output_path_ppm):
+    kernel(lat_range, lon_range, kernel_path,
+           model_path, output_path_d660, output_path_ppm)
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
