4f07d68993616cdfc78eb0caa417ad414a6aadf1
ziyixi <ziyixi@mail.ustc.edu.cn>
2021-05-31 19:56:01 -0400

    add the script to calculate misfit based on misfit_windows and the weights

--- seisflow/scripts/helper/calculate_misfit_from_misfit_and_weight.py
+++ seisflow/scripts/helper/calculate_misfit_from_misfit_and_weight.py
@@ -20,6 +20,24 @@ def read_files(misfit_directory,weight_file):
 def cal_overall_misfit(weights,misfits):
     overall_misfit=0
     overall_weight=0
+    # 6 categories
+    misfit_categories={
+        "z":0,
+        "r":0,
+        "t":0,
+        "surface_z":0,
+        "surface_r":0,
+        "surface_t":0
+    }
+    weight_categories={
+        "z":0,
+        "r":0,
+        "t":0,
+        "surface_z":0,
+        "surface_r":0,
+        "surface_t":0
+    }
+
     for each_event in weights:
         for each_station in weights[each_event]:
             for each_category in weights[each_event][each_station]:
@@ -29,16 +47,20 @@ def cal_overall_misfit(weights,misfits):
                     themisfit=1-misfit_win.similarity
                     overall_misfit+=theweight*themisfit
                     overall_weight+=theweight
+                    misfit_categories[each_category]+=theweight*themisfit
+                    weight_categories[each_category]+=theweight
     overall_misfit=overall_misfit/overall_weight
-    return overall_misfit
+    for each_category in ["z","r","t","surface_z","surface_r","surface_t"]:
+        misfit_categories[each_category]=misfit_categories[each_category]/weight_categories[each_category]
+    return overall_misfit,misfit_categories["z"],misfit_categories["r"],misfit_categories["t"],misfit_categories["surface_z"],misfit_categories["surface_r"],misfit_categories["surface_t"]
 
 @click.command()
 @click.option('--misfit_directory', required=True, type=str, help="the misfit directory")
 @click.option('--weight_file', required=True, type=str, help="the weight file")
 def main(misfit_directory,weight_file):
     weights,misfits=read_files(misfit_directory,weight_file)
-    overall_misfit=cal_overall_misfit(weights,misfits)
-    print(overall_misfit)
+    result=cal_overall_misfit(weights,misfits)
+    print(result)
 
 if __name__ == "__main__":
     main()  # pylint: disable=no-value-for-parameter
