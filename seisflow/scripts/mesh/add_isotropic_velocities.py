31e08616e4f8bba26b13014e5ebbbbb83743c4d1
ziyixi <ziyixi@mail.ustc.edu.cn>
2021-06-13 15:11:19 -0400

    update mesh related scripts

--- seisflow/scripts/mesh/add_isotropic_velocities.py
+++ seisflow/scripts/mesh/add_isotropic_velocities.py
@@ -3,29 +3,24 @@ add_isotropic_velocities.py: add vp and vs velocity for the absolute velocity ne
 """
 import click
 import numpy as np
-from netCDF4 import Dataset
+import xarray as xr
 
 
 @click.command()
 @click.option('--target_netcdf', required=True, type=str)
 def main(target_netcdf):
-    with Dataset(target_netcdf, 'a') as f:
-        # * vsv,vsh,vpv,vph must exist in the file, and we generate vs and vp.
-        vsv = f.variables["vsv"][:]
-        vsh = f.variables["vsh"][:]
-        vpv = f.variables["vpv"][:]
-        vph = f.variables["vph"][:]
-        # calculate
-        # reference  doi: 10.1111/j.1365-246X.2006.03100.x
-        vs = np.sqrt((2 * vsv ** 2 + vsh ** 2) / 3)
-        vp = np.sqrt((vpv ** 2 + 4 * vph ** 2) / 5)
-        # write vp and vs to the netcdf file
-        netcdf_vp = f.createVariable(
-            "vp", 'f8', ('longitude', 'latitude', 'depth'), zlib=True)
-        netcdf_vs = f.createVariable(
-            "vs", 'f8', ('longitude', 'latitude', 'depth'), zlib=True)
-        netcdf_vp[:] = vp
-        netcdf_vs[:] = vs
+    with xr.open_dataset(target_netcdf) as data:
+        data.load()
+    vsv = data["vsv"].data
+    vsh = data["vsh"].data
+    vpv = data["vpv"].data
+    vph = data["vph"].data
+    # reference  doi: 10.1111/j.1365-246X.2006.03100.x
+    vs = np.sqrt((2 * vsv ** 2 + vsh ** 2) / 3)
+    vp = np.sqrt((vpv ** 2 + 4 * vph ** 2) / 5)
+    data["vs"] = (('longitude', 'latitude', 'depth'), vs)
+    data["vp"] = (('longitude', 'latitude', 'depth'), vp)
+    data.to_netcdf(target_netcdf)
 
 
 if __name__ == "__main__":
--- seisflow/scripts/mesh/make_perturbed_netcdf.py
+++ seisflow/scripts/mesh/make_perturbed_netcdf.py
@@ -1,7 +1,7 @@
 """
 make_perturbed_netcdf.py: make the perturbed netcdf based on two nc files.
 """
-from netCDF4 import Dataset
+import xarray as xr
 import click
 import numpy as np
 
@@ -13,38 +13,12 @@ import numpy as np
 @click.option('--models', required=False, default="vpv,vph,vsv,vsh,eta,rho", type=str)
 def main(target_netcdf, reference_netcdf, output_netcdf, models):
     models = models.split(",")
-    with Dataset(target_netcdf, 'r') as target:
-        with Dataset(reference_netcdf, 'r') as reference:
-            with Dataset(output_netcdf, 'w') as f:
-                history = f"perturbation for {target_netcdf} respect to {reference_netcdf} "
-                f.history = history
-                f.createDimension(
-                    'depth', target.variables["depth"][:].shape[0])
-                f.createDimension(
-                    'latitude', target.variables["latitude"][:].shape[0])
-                f.createDimension(
-                    'longitude', target.variables["longitude"][:].shape[0])
-                longitude = f.createVariable('longitude', 'f8', ('longitude',))
-                longitude[:] = target.variables["longitude"][:]
-                latitude = f.createVariable('latitude', 'f8', ('latitude',))
-                latitude[:] = target.variables["latitude"][:]
-                depth = f.createVariable('depth', 'f8', ('depth',))
-                depth[:] = target.variables["depth"][:]
-                for index_parameter in range(len(models)):
-                    parameter_name = models[index_parameter]
-                    # get parameter_array
-                    target_array = target.variables[parameter_name][:].copy()
-                    reference_array = reference.variables[parameter_name][:].copy(
-                    )
-                    # target_array[target_array > 9e6] = np.nan
-                    # reference_array[reference_array > 9e6] = np.nan
-                    parameter_array = (
-                        target_array - reference_array) / reference_array
-                    parameter_array[target.variables[parameter_name]
-                                    [:] > 9e6] = 9999999.
-                    netcdf_var = f.createVariable(
-                        parameter_name, 'f8', ('longitude', 'latitude', 'depth'), zlib=True)
-                    netcdf_var[:] = parameter_array
+    with xr.open_dataset(target_netcdf) as target:
+        with xr.open_dataset(reference_netcdf) as reference:
+            for each_parameter in models:
+                target[each_parameter].data = target[each_parameter].data / \
+                    reference[each_parameter].data-1
+            target.to_netcdf(output_netcdf)
 
 
 if __name__ == "__main__":
